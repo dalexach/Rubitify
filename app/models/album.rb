c2cee609bbe410f8deeb92586aeb6aecbb27ad2b
dalexach <dalexach@gmail.com>
2020-11-20 21:47:13 -0500

    Typo correction

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
   has_many :songs, dependent: :delete_all
   belongs_to :artist
-  validates_precence_of :name, :spotify_url, :total_tracks, :spotify_id, :artist_id
+  validates_presence_of :name, :spotify_url, :total_tracks, :spotify_id, :artist_id
 end
