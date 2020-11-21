00686628add0cf4e8f119d5451045a6f288d754e
dalexach <dalexach@gmail.com>
2020-11-20 19:24:09 -0500

    Uploading the configuration of the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+  namespace :api do
+    namespace :v1 do
+      resources :artists, only: %i[index] do
+        get :albums, on: :member
+      end
+      resources :albums, only: [] do
+        get :song, on: :member
+      end
+      get '/genres/:genre_name/random_song' => 'genres#random_song'
+    end
+  end
 end
