e0fb5fdde303127d7263eb2cc410b822138cf5cb
dalexach <dalexach@gmail.com>
2020-11-20 22:23:29 -0500

    Change some configuration on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  namespace :api do
-    namespace :v1 do
-      resources :artists, only: %i[index] do
-        get :albums, on: :member
-      end
-      resources :albums, only: [] do
-        get :song, on: :member
-      end
-      get '/genres/:genre_name/random_song' => 'genres#random_song'
+  namespace "api" do
+    namespace "v1" do
+      resources :artists, only: [:index] 
+
+      get "artists/:id/albums", to: "artists#albums"
+
+      get "albums", to: "albums#index"
+      get "albums/:id/songs", to: "albums#songs" 
+      get "genres/:genre_name/random_song", to: "songs#random_song"
     end
   end
 end
